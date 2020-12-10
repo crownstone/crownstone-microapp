6f4a140a8090c903ef1ffdf04b9517a41570d44f
Anne van Rossum <anne@crownstone.rocks>
2020-12-10 13:50:13 +0100

    Remove lots of structs. Add dependency on shared/cs_MicroappStructs.h

--- include/microapp.h
+++ include/microapp.h
@@ -1,51 +1,21 @@
 #pragma once
 
-#include <stdint.h>
+#include <cs_MicroappStructs.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-const uint8_t MAX_PAYLOAD = 32;
+#define OUTPUT CS_MICROAPP_COMMAND_PIN_WRITE
+#define INPUT CS_MICROAPP_COMMAND_PIN_READ
+#define TOGGLE CS_MICROAPP_COMMAND_PIN_TOGGLE
+#define I2C_READ CS_MICROAPP_COMMAND_PIN_I2C_READ
+#define I2C_WRITE CS_MICROAPP_COMMAND_PIN_I2C_WRITE
 
-/*
- * The structure used for communication.
- */
-typedef struct {
-	uint8_t payload[MAX_PAYLOAD];
-	int length;
-} message_t;
+#define HIGH CS_MICROAPP_COMMAND_SWITCH_ON
+#define LOW CS_MICROAPP_COMMAND_SWITCH_OFF
 
-
-enum CommandMicroapp {
-	CS_MICROAPP_COMMAND_LOG          = 0x01,
-	CS_MICROAPP_COMMAND_DELAY        = 0x02,
-	CS_MICROAPP_COMMAND_PIN          = 0x03,
-	CS_MICROAPP_COMMAND_SERVICE_DATA = 0x04,
-};
-
-/*
- * Struct to set and read pins. This can be used for analog and digital writes and reads. For digital writes it is
- * just zeros or ones. For analog writes it is an uint8_t. For reads the value field is the one that is being returned.
- */
-typedef struct {
-	uint8_t cmd;
-	uint8_t pin;
-	uint8_t opcode;
-	uint8_t value;
-} pin_cmd_t;
-
-enum CommandMicroappPinOpcde {
-	CS_MICROAPP_COMMAND_READ         = 0x01,
-	CS_MICROAPP_COMMAND_WRITE        = 0x02,
-	CS_MICROAPP_COMMAND_TOGGLE       = 0x03,
-};
-
-typedef struct {
-	uint8_t cmd;
-	uint16_t period;
-	uintptr_t coargs;
-} sleep_cmd_t;
+#define NUMBER_OF_PINS 2
 
 /*
  * To save space have a single global message object.
--- src/Arduino.c
+++ src/Arduino.c
@@ -1,41 +1,53 @@
 #include <Arduino.h>
 #include <microapp.h>
+
+// Shared header files with bluenet
+//#include <cs_MicroappStructs.h>
 #include <ipc/cs_IpcRamData.h>
 
-void pinMode(uint8_t pin, uint8_t mode) {
+static uint8_t pin_modes[NUMBER_OF_PINS];
+
+bool pinExists(uint8_t pin) {
+	return (pin < NUMBER_OF_PINS);
 }
 
-// TODO: use pin_cmd_t
+// TODO: check also correctness of "mode"
+void pinMode(uint8_t pin, uint8_t mode) {
+	if (!pinExists(pin)) return;
+	pin_modes[pin] = mode;
+}
+
 void digitalWrite(uint8_t pin, uint8_t val) {
+	if (!pinExists(pin)) return;
 
-	global_msg.payload[0] = CS_MICROAPP_COMMAND_PIN;
-	global_msg.payload[1] = pin;
-	global_msg.payload[2] = val;
-	global_msg.length = 3;
-
-	// TODO, add opcode at loc 2, like this or preferably use struct (and assume same endianness)
-	// global_msg.payload[2] = CS_MICROAPP_COMMAND_WRITE;
-	// global_msg.payload[3] = val;
-	// global_msg.length = 4;
+	pin_cmd_t *pin_cmd = (pin_cmd_t*)&global_msg;
+	pin_cmd->cmd = CS_MICROAPP_COMMAND_PIN;
+	pin_cmd->pin = pin;
+	pin_cmd->opcode = pin_modes[pin];
+	pin_cmd->value = val;
+	global_msg.length = sizeof(pin_cmd_t);
 
 	sendMessage(global_msg);
 }
 
 int digitalRead(uint8_t pin) {
-
-	global_msg.payload[0] = CS_MICROAPP_COMMAND_PIN;
-	global_msg.payload[1] = pin;
-	global_msg.payload[2] = CS_MICROAPP_COMMAND_WRITE;
-	global_msg.payload[3] = 0;
-	global_msg.length = 4;
+	if (!pinExists(pin)) return -1;
+	
+	pin_cmd_t *pin_cmd = (pin_cmd_t*)&global_msg;
+	pin_cmd->cmd = CS_MICROAPP_COMMAND_PIN;
+	pin_cmd->pin = pin;
+	pin_cmd->opcode = pin_modes[pin];
+	pin_cmd->value = 0;
+	global_msg.length = sizeof(pin_cmd_t);
 	
 	sendMessage(global_msg);
 
-	// TODO, perhaps larger type then uint8_t
-	uint8_t value = global_msg.payload[3];
+	// TODO, perhaps a larger type then uint8_t is required / desired
+	uint8_t value = pin_cmd->value;
 	return value;
 }
 
+// Internally it is just the same as digitalRead
 int analogRead(uint8_t pin) {
 	return digitalRead(pin);
 }
@@ -43,6 +55,7 @@ int analogRead(uint8_t pin) {
 void analogReference(uint8_t mode) {
 }
 
+// Internally it is just the same as digitalWrite
 void analogWrite(uint8_t pin, int val) {
 	digitalWrite(pin, val);
 }
