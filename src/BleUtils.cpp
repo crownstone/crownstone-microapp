7716de20e3879dc6c62a0ae0d96134568b34af29
Martijn van der Marel <martijn.vander.marel@hetnet.nl>
2021-12-08 11:58:16 +0100

    code optimization in BleUtils

--- src/BleUtils.cpp
+++ src/BleUtils.cpp
@@ -3,18 +3,16 @@
 // Convert a pair of chars to a byte, e.g. convert "A3" to 0xA3
 uint8_t convertTwoHexCharsToByte(const char* chars) {
 	uint8_t val[2] = {0, 0}; // actually two 4-bit values
-	char* p = (char*) chars;
 	for (uint8_t i = 0; i < 2; i++) {
-		if (*p >= '0' && *p <= '9') {
-			val[i] = *p - '0';
+		if (chars[i] >= '0' && chars[i] <= '9') {
+			val[i] = chars[i] - '0';
 		}
-		else if (*p >= 'a' && *p <= 'f') {
-			val[i] = *p - 'a' + 10;
+		else if (chars[i] >= 'a' && chars[i] <= 'f') {
+			val[i] = chars[i] - 'a' + 10;
 		}
-		else if (*p >= 'A' && *p <= 'F') {
-			val[i] = *p - 'A' + 10;
+		else if (chars[i] >= 'A' && chars[i] <= 'F') {
+			val[i] = chars[i] - 'A' + 10;
 		}
-		p++;
 	}
 	// shift most significant 4-bit value 4 bits to the left and add least significant 4-bit value
 	return ((val[0] & 0x0F) << 4) | (val[1] & 0x0F);
@@ -24,15 +22,13 @@ void convertByteToTwoHexChars(uint8_t byte, char* res) {
 	uint8_t c[2]; // divide into two 4-bit numbers
 	c[0] = (byte >> 4) & 0x0F;
 	c[1] = byte & 0x0F;
-	uint8_t* p = c;
 	for (uint8_t i = 0; i < 2; i++) {
-		if (*p >= 0 && *p <= 9) {
-			*res = *p + '0';
+		if (c[i] >= 0 && c[i] <= 9) {
+			*res = c[i] + '0';
 		}
-		else if (*p >= 0xA && *p <= 0xF) {
-			*res = *p + 'A' - 10;
+		else if (c[i] >= 0xA && c[i] <= 0xF) {
+			*res = c[i] + 'A' - 10;
 		}
-		p++;
 		res++;
 	}
 }
@@ -45,20 +41,15 @@ MACaddress convertStringToMac(const char* mac_str) {
 	if (len != MAC_ADDRESS_STRING_LENGTH) {
 		return mac;  // input string not of correct size
 	}
-	char* p = (char*) mac_str;
 	for (uint8_t i = 0; i < MAC_ADDRESS_LENGTH; i++) {
-		mac.byte[i] = convertTwoHexCharsToByte(p);
-		p += 3;  // skip 3 chars and go to the next pair of chars
+		mac.byte[i] = convertTwoHexCharsToByte(mac_str + 3 * i);
 	}
 	return mac;
 };
 
 void convertMacToString(MACaddress mac, char* res) {
 	for (uint8_t i = 0; i < MAC_ADDRESS_LENGTH; i++) {
-		char c[2] = {'0','0'};
-		convertByteToTwoHexChars(mac.byte[i], c);
-		res[3*i] = c[0];
-		res[3*i+1] = c[1];
+		convertByteToTwoHexChars(mac.byte[i], res + 3 * i);
 		res[3*i+2] = ':';
 	}
 	// replace last colon with a terminating character
@@ -67,10 +58,8 @@ void convertMacToString(MACaddress mac, char* res) {
 
 uuid16_t convertStringToUuid(const char* uuid_str) {
 	uint8_t byte[2];
-	char* p = (char*) uuid_str;
 	for (uint8_t i = 0; i < 2; i++) {
-		byte[i] = convertTwoHexCharsToByte(p);
-		p += 2;
+		byte[i] = convertTwoHexCharsToByte(uuid_str + 2 * i);
 	}
 	uuid16_t res = (byte[0] << 8) | (byte[1] & 0xFF);
 	return res;
