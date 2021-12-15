aacd5cd00dd5dc3fec5b633b452b072173b574ce
m <m.plagge@protonmail.com>
2021-12-15 16:26:00 +0100

    Updated comment

--- include/Presence.h
+++ include/Presence.h
@@ -4,6 +4,9 @@
 
 class Presence{
 public:
+	// Check if Profile is in sphere
 	uint64_t getPresence(uint8_t profileId);
+
+	// Check if profile is in a Room 
 	bool isPresent(uint8_t profileId, uint8_t roomId);
 };
