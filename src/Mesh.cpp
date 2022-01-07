6a18adfd20ee0ebe727af5652486c5f83d1c5e08
m <m.plagge@protonmail.com>
2022-01-07 09:58:42 +0100

    Fixed code style

--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -3,7 +3,7 @@
 static const uint8_t CMD_SIZE = 1;
 uint8_t meshHeaderSize        = sizeof(microapp_mesh_header_t);
 
-void Mesh::sendMeshMsg(uint8_t* msg, uint8_t msg_size, uint8_t stoneId) {
+void Mesh::sendMeshMsg(uint8_t* msg, uint8_t msgSize, uint8_t stoneId) {
 
 	uint8_t meshHeaderSendSize = sizeof(microapp_mesh_send_header_t);
 
@@ -16,13 +16,13 @@ void Mesh::sendMeshMsg(uint8_t* msg, uint8_t msg_size, uint8_t stoneId) {
 			(microapp_mesh_send_header_t*)&global_msg.payload[CMD_SIZE + meshHeaderSize];
 	send_mesh_cmd_header->stoneId = stoneId;
 
-	memcpy(&global_msg.payload[CMD_SIZE + meshHeaderSize + meshHeaderSendSize], msg, msg_size);
+	memcpy(&global_msg.payload[CMD_SIZE + meshHeaderSize + meshHeaderSendSize], msg, msgSize);
 
-	global_msg.length = CMD_SIZE + meshHeaderSize + meshHeaderSendSize + msg_size;
+	global_msg.length = CMD_SIZE + meshHeaderSize + meshHeaderSendSize + msgSize;
 	sendMessage(&global_msg);
 }
 
-uint8_t Mesh ::readMeshMsg(uint8_t** msg_ptr, uint8_t* stone_id_ptr) {
+uint8_t Mesh ::readMeshMsg(uint8_t** msgPtr, uint8_t* stoneIdPtr) {
 
 	uint8_t meshHeaderReadSize = sizeof(microapp_mesh_read_t);
 
@@ -30,9 +30,8 @@ uint8_t Mesh ::readMeshMsg(uint8_t** msg_ptr, uint8_t* stone_id_ptr) {
 	microapp_mesh_header_t* read_mesh_cmd_opcode = (microapp_mesh_header_t*)&global_msg.payload[CMD_SIZE];
 	read_mesh_cmd_opcode->opcode                 = CS_MICROAPP_COMMAND_MESH_READ;
 
-	microapp_mesh_read_t* read_mesh_cmd_header =
-			(microapp_mesh_read_t*)&global_msg.payload[CMD_SIZE + meshHeaderSize];
-	read_mesh_cmd_header->messageSize = 0;
+	microapp_mesh_read_t* read_mesh_cmd_header = (microapp_mesh_read_t*)&global_msg.payload[CMD_SIZE + meshHeaderSize];
+	read_mesh_cmd_header->messageSize          = 0;
 
 	global_msg.length = CMD_SIZE + meshHeaderSize + meshHeaderReadSize;
 	sendMessage(&global_msg);
