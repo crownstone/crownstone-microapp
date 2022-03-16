76e02b0279905e65c49baccf4ee203e88ffb0bee
mrquincle <anne@crownstone.rocks>
2022-03-16 17:38:42 +0100

    Return number of empty slots in queue
    More robust method to decided if there's space for a new interrupt to be
    handled by the microapp than counting on the bluenet side.

--- src/microapp.c
+++ src/microapp.c
@@ -1,5 +1,5 @@
-#include <microapp.h>
 #include <ipc/cs_IpcRamData.h>
+#include <microapp.h>
 
 // Define array with soft interrupts
 soft_interrupt_t softInterrupt[MAX_SOFT_INTERRUPTS];
@@ -10,7 +10,7 @@ soft_interrupt_t softInterrupt[MAX_SOFT_INTERRUPTS];
 // returns size MAX_STRING_SIZE for strings that are too long, note that this can still not fit in the payload
 // the actually supported string length depends on the opcode
 // the limit here is just to prevent looping forever
-uint8_t strlen(const char *str) {
+uint8_t strlen(const char* str) {
 	for (size_t i = 0; i < MAX_STRING_SIZE; ++i) {
 		if (str[i] == 0) {
 			return i;
@@ -23,17 +23,17 @@ uint8_t strlen(const char *str) {
 // returns 0 if ptr1 and ptr2 are equal
 // returns -1 if for the first unmatching byte i we have ptr1[i] < ptr2[i]
 // returns 1 if for the first unmatching byte i we have ptr1[i] > ptr2[i]
-int memcmp(const void *ptr1, const void *ptr2, size_t num) {
-	char *p = (char*) ptr1;
-	char *q = (char*) ptr2;
-	if (ptr1 == ptr2) { // point to the same address
+int memcmp(const void* ptr1, const void* ptr2, size_t num) {
+	char* p = (char*)ptr1;
+	char* q = (char*)ptr2;
+	if (ptr1 == ptr2) {  // point to the same address
 		return 0;
 	}
 	for (size_t i = 0; i < num; ++i) {
-		if (*(p+i) < *(q+i)) {
+		if (*(p + i) < *(q + i)) {
 			return -1;
 		}
-		else if (*(p+i) > *(q+i)) {
+		else if (*(p + i) > *(q + i)) {
 			return 1;
 		}
 	}
@@ -41,8 +41,8 @@ int memcmp(const void *ptr1, const void *ptr2, size_t num) {
 }
 
 void* memcpy(void* dest, const void* src, size_t num) {
-	uint8_t* p = (uint8_t*) src;
-	uint8_t* q = (uint8_t*) dest;
+	uint8_t* p = (uint8_t*)src;
+	uint8_t* q = (uint8_t*)dest;
 	for (size_t i = 0; i < num; ++i) {
 		*q = *p;
 		p++;
@@ -64,14 +64,14 @@ bluenet2microapp_ipcdata_t ipc_data;
 /*
  * Get the outgoing message buffer.
  */
-io_buffer_t *getOutgoingMessageBuffer() {
+io_buffer_t* getOutgoingMessageBuffer() {
 	return &io_buffer.microapp2bluenet;
 }
 
 /*
  * Get the incoming message buffer.
  */
-io_buffer_t *getIncomingMessageBuffer() {
+io_buffer_t* getIncomingMessageBuffer() {
 	return &io_buffer.bluenet2microapp;
 }
 
@@ -104,8 +104,8 @@ int checkRamData(bool checkOnce) {
 		}
 	}
 	uint8_t rd_size = 0;
-	uint8_t ret_code = 
-		getRamData(IPC_INDEX_CROWNSTONE_APP, (uint8_t*)&ipc_data, sizeof(bluenet2microapp_ipcdata_t), &rd_size);
+	uint8_t ret_code =
+			getRamData(IPC_INDEX_CROWNSTONE_APP, (uint8_t*)&ipc_data, sizeof(bluenet2microapp_ipcdata_t), &rd_size);
 
 	if (ret_code != 0) {
 		return result;
@@ -115,7 +115,7 @@ int checkRamData(bool checkOnce) {
 		return result;
 	}
 
-	if(ipc_data.protocol != 1) {
+	if (ipc_data.protocol != 1) {
 		return result;
 	}
 
@@ -124,7 +124,7 @@ int checkRamData(bool checkOnce) {
 	}
 
 	ipc_data.valid = true;
-	
+
 	if (checkOnce) {
 		// Write the buffer only once
 		microappCallbackFunc callbackFunctionIntoBluenet = ipc_data.microappCallback;
@@ -134,56 +134,90 @@ int checkRamData(bool checkOnce) {
 }
 
 /*
- * Handle incoming requests from bluenet (probably soft interrupts).
+ * Returns the number of empty slots for bluenet. Access and counting of empty slots can be improved.
  */
-int handleBluenetRequest(microapp_cmd_t *request) {
-	int result = 1;
-
-	// There is no actual request (no problem)
-	if (request->ack != CS_ACK_BLUENET_MICROAPP_REQUEST) {
-		return 0;
-	}
-
-	// Check if there is an empty slot for a request/command
-	queue_t *localCopy = nullptr;
-	int8_t nextIndex = -1;
+int8_t emptySlotsInQueue() {
+	queue_t* localCopy = nullptr;
+	int8_t totalEmpty  = 0;
 	for (int8_t i = 0; i < MAX_QUEUE; ++i) {
 		localCopy = &localQueue[i];
 		if (!localCopy->filled) {
-			nextIndex = i;
+			totalEmpty++;
+		}
+	}
+	return totalEmpty;
+}
+
+/*
+ * Gets a new slide.
+ */
+int8_t getNewItemInQueue() {
+	queue_t* localCopy = nullptr;
+	int8_t queueIndex  = -1;
+	for (int8_t i = 0; i < MAX_QUEUE; ++i) {
+		localCopy = &localQueue[i];
+		if (!localCopy->filled) {
+			queueIndex        = i;
 			localCopy->filled = true;
 			break;
 		}
 	}
+	return queueIndex;
+}
+
+/*
+ * Handle incoming requests from bluenet (probably soft interrupts).
+ */
+int handleBluenetRequest(microapp_cmd_t* cmd) {
+	int result = 1;
+
+	// There is no actual request (no problem, just return)
+	if (cmd->ack != CS_ACK_BLUENET_MICROAPP_REQUEST) {
+		return 0;
+	}
+
+	microapp_soft_interrupt_cmd_t* request = reinterpret_cast<microapp_soft_interrupt_cmd_t*>(cmd);
+
+	// Check if there are empty slots
+	int8_t emptySlots            = emptySlotsInQueue();
+	request->emptyInterruptSlots = emptySlots;
+
+	int8_t queueIndex = -1;
+	if (emptySlots > 0) {
+		queueIndex = getNewItemInQueue();
+	}
+
 	// overwrite in both scenarios the REQUEST
-	if (nextIndex < 0) {
-		request->ack = CS_ACK_BLUENET_MICROAPP_REQ_BUSY;
-	} else {
-		request->ack = CS_ACK_BLUENET_MICROAPP_REQ_ACK;
+	if (queueIndex < 0) {
+		request->header.ack = CS_ACK_BLUENET_MICROAPP_REQ_BUSY;
+	}
+	else {
+		request->header.ack = CS_ACK_BLUENET_MICROAPP_REQ_ACK;
 	}
 
 	// First indicate we have received the callback
-	microapp_cmd_t *outputBuffer = reinterpret_cast<microapp_cmd_t*>(io_buffer.microapp2bluenet.payload);
-	if (nextIndex < 0) {
+	microapp_cmd_t* outputBuffer = reinterpret_cast<microapp_cmd_t*>(io_buffer.microapp2bluenet.payload);
+	if (queueIndex < 0) {
 		outputBuffer->cmd = CS_MICROAPP_COMMAND_SOFT_INTERRUPT_ERROR;
-	} else {
+	}
+	else {
 		outputBuffer->cmd = CS_MICROAPP_COMMAND_SOFT_INTERRUPT_RECEIVED;
 	}
 	microappCallbackFunc callbackFunctionIntoBluenet = ipc_data.microappCallback;
 	result = callbackFunctionIntoBluenet(CS_MICROAPP_CALLBACK_SIGNAL, &io_buffer);
 
-	if (nextIndex < 0) {
-		// TODO: Make sure that this return is not screwing up counters at the bluenet side.
+	if (queueIndex < 0) {
 		return -1;
 	}
 
 	// Continue execution, this will end up with a call to sendMessage() but it will not enter this function because
 	// the microapp itself changed request->ack to something else than CS_ACK_BLUENET_MICROAPP_REQUEST.
 
+	queue_t* localCopy = &localQueue[queueIndex];
 	memcpy(localCopy->buffer, request, MAX_PAYLOAD);
 	microapp_cmd_t* msg = reinterpret_cast<microapp_cmd_t*>(localCopy->buffer);
-	result = handleSoftInterrupt(msg);
-	localCopy->filled = false;
+	result              = handleSoftInterrupt(msg);
+	localCopy->filled   = false;
 	return result;
 }
 
@@ -204,22 +238,21 @@ int sendMessage() {
 	result = callbackFunctionIntoBluenet(CS_MICROAPP_CALLBACK_SIGNAL, &io_buffer);
 
 	// Here the microapp resumes execution, check for incoming messages
-	microapp_cmd_t *inputBuffer = reinterpret_cast<microapp_cmd_t*>(io_buffer.bluenet2microapp.payload);
+	microapp_cmd_t* inputBuffer = reinterpret_cast<microapp_cmd_t*>(io_buffer.bluenet2microapp.payload);
 
 	result = handleBluenetRequest(inputBuffer);
 
 	return result;
 }
 
-
-void registerSoftInterrupt(soft_interrupt_t *interrupt) {
+void registerSoftInterrupt(soft_interrupt_t* interrupt) {
 	for (int i = 0; i < MAX_SOFT_INTERRUPTS; ++i) {
 		if (!softInterrupt[i].registered) {
-			softInterrupt[i].registered = true;
+			softInterrupt[i].registered        = true;
 			softInterrupt[i].softInterruptFunc = interrupt->softInterruptFunc;
-			softInterrupt[i].id = interrupt->id;
-			softInterrupt[i].arg = interrupt->arg;
-			softInterrupt[i].type = interrupt->type;
+			softInterrupt[i].id                = interrupt->id;
+			softInterrupt[i].arg               = interrupt->arg;
+			softInterrupt[i].type              = interrupt->type;
 			break;
 		}
 	}
@@ -245,7 +278,7 @@ int countRegisteredSoftInterrupts(uint8_t type) {
 	return result;
 }
 
-int handleSoftInterruptInternal(uint8_t type, uint8_t id, uint8_t *msg) {
+int handleSoftInterruptInternal(uint8_t type, uint8_t id, uint8_t* msg) {
 	for (int i = 0; i < MAX_SOFT_INTERRUPTS; ++i) {
 		if (!softInterrupt[i].registered || softInterrupt[i].type != type) {
 			continue;
@@ -261,21 +294,21 @@ int handleSoftInterruptInternal(uint8_t type, uint8_t id, uint8_t *msg) {
 	return -1;
 }
 
-int handleSoftInterrupt(microapp_cmd_t *msg) {
+int handleSoftInterrupt(microapp_cmd_t* msg) {
 	int result = 0;
-	switch(msg->interruptCmd) {
-	case CS_MICROAPP_COMMAND_BLE_DEVICE: {
-		result = handleSoftInterruptInternal(SOFT_INTERRUPT_TYPE_BLE, msg->id, (uint8_t*)msg);
-		break;
-	}
-	case CS_MICROAPP_COMMAND_PIN: {
-		microapp_pin_cmd_t* cmd = reinterpret_cast<microapp_pin_cmd_t*>(msg);
-		if (cmd->value != CS_MICROAPP_COMMAND_VALUE_CHANGE) {
+	switch (msg->interruptCmd) {
+		case CS_MICROAPP_COMMAND_BLE_DEVICE: {
+			result = handleSoftInterruptInternal(SOFT_INTERRUPT_TYPE_BLE, msg->id, (uint8_t*)msg);
 			break;
 		}
-		result = handleSoftInterruptInternal(SOFT_INTERRUPT_TYPE_PIN, cmd->pin, (uint8_t*)msg);
-		break;
-	}
+		case CS_MICROAPP_COMMAND_PIN: {
+			microapp_pin_cmd_t* cmd = reinterpret_cast<microapp_pin_cmd_t*>(msg);
+			if (cmd->value != CS_MICROAPP_COMMAND_VALUE_CHANGE) {
+				break;
+			}
+			result = handleSoftInterruptInternal(SOFT_INTERRUPT_TYPE_PIN, cmd->pin, (uint8_t*)msg);
+			break;
+		}
 	}
 	return result;
 }
